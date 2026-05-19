26db0dcb4d4323ff9710a24e8d189a5467e26602
Yeath334 <3405316311@qq.com>
2026-05-19 22:01:20 +0800

    Delete clim_verification.sh

--- clim_verification.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -p kshdmcc2026
-#SBATCH -N 1
-#SBATCH -n 4  
-#SBATCH --exclusive 
-#SBATCH --gres=dcu:1  
-#SBATCH --time=02:00:00  
-export PATH=/public/home/achwjznh4b/install//bin:$PATH
-time matlab -nodisplay -nosplash -nodesktop < clim_verification.m
